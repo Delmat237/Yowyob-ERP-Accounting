21db879c1b034e0f7cf1a1cf7d0a881936d60ac7
Delmat237 <azangueleonel9@gmail.com>
2026-07-08 10:34:19 +0100

    feat : avatar

--- components/layout/user-nav.tsx
+++ components/layout/user-nav.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
